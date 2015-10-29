ae580560eec3bc56c9f4ba4d9deb49323878733e
Andrew DeChristopher <dechristophera@wit.edu>
2015-10-28 23:11:44 -0400

    Created initial matrix.

--- cauldron/src/net/kiirus/cauldron/Cauldron.java
+++ cauldron/src/net/kiirus/cauldron/Cauldron.java
@@ -1,14 +1,24 @@
 package net.kiirus.cauldron;
 
 public class Cauldron {
-
-	public Cauldron() {
-		// TODO Auto-generated constructor stub
-	}
+	
+	/**
+	 * This is the 4x4 grid that all encryption algorithms will
+	 * calculate on.
+	 */
+	public static String[][] a = new String[][]{
+		{"", "", "", ""},
+		{"", "", "", ""},
+		{"", "", "", ""},
+		{"", "", "", ""}
+	};
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
+	
+	
 
 }
